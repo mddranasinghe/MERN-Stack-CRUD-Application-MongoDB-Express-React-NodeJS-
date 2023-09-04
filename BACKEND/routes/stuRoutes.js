1b82a912cb3bf5fb7b06c3cd93e4743d867a404c
mddranasinghe <128118056+mddranasinghe@users.noreply.github.com>
2023-09-04 10:50:10 +0530

    Update stuRoutes.js

--- BACKEND/routes/stuRoutes.js
+++ BACKEND/routes/stuRoutes.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 
 let Student = require("../Model/students.js");
 
+
+// add students...........................................
 router.route("/add").post((req,res) => {
 
 const name= req.body.name;
@@ -38,6 +40,34 @@ router,route("/").get((req,res)=>{
 
 });
 
+//update ..........................................................
+router.route("/upadate/:id").put(async(req,res)=>
+{
+let userId=req.params.id; // ulr eke parameter ekk vidihat ena id ek asign krgnnva userId kt
 
-router.route("/")
+//desturcture
+const {name,gender,age} = req.body;
+
+const updateStudents ={
+    name,
+    age,
+    gender,
+};
+const update=await Student.findByIdAndUpdate(userId,updateStudents).then
+(()=>{
+res.status(200).send({status :"user updated",user:update})
+}).catch((err)=>{
+
+    console.log(err);
+    res.status(500).send({status:"Error with updating data"});
+});
+});
+
+
+
+
+
+
+
+
 module.exports =router;
